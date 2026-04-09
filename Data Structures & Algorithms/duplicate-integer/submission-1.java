a66909fead85b90e85d80e804f3c99c6c87ef328
Tannushree Vaishnav <167300196+TannushreeVaishnav@users.noreply.github.com>
2026-04-10 00:36:36 +0530

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        boolean flag=false;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                flag=true;
+                break;
+            }
+        }
+        return flag;
+        
+    }
+}
