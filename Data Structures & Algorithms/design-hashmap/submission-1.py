d2304031d991cbeadbb777d94df4876b6d4c9176
Tannushree Vaishnav <167300196+TannushreeVaishnav@users.noreply.github.com>
2026-05-06 16:25:52 +0530

    Add: design-hashmap - submission-1

---
+++ Data Structures & Algorithms/design-hashmap/submission-1.py
@@ -0,0 +1,24 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.map=[0]*10000000
+        
+
+    def put(self, key: int, value: int) -> None:
+        self.map[key]=value+1
+        
+
+    def get(self, key: int) -> int:
+        return self.map[key]-1
+        
+
+    def remove(self, key: int) -> None:
+        self.map[key]=0
+        
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
